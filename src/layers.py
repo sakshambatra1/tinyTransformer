53aae5a19a6e85c279f080a490be250938755fb1
sakshambatra1 <batrasaksham2005@gmail.com>
2025-10-16 16:15:16 -0400

    finished layers with encoder, decoder, and all the blocks

--- src/layers.py
+++ src/layers.py
@@ -1,20 +1,20 @@
 import jax.numpy as jnp 
 import jax 
 from jax import random 
-from selfattention import multihead_attn
+from selfattention import multihead_attn, masked_multihead_attn
 
 def relu(x:jax.Array):
     return jnp.maximum(0,x)
 
 class LayerNorm: 
-    def __init__(self, dim, eps=1e-15): 
+    def __init__(self, dim, eps=1e-5): 
         self.eps = eps 
         self.gamma = jnp.ones((dim,))
         self.beta = jnp.zeros((dim,))
 
     def __call__(self, x):
         mean = jnp.mean(x, axis=-1, keepdims=True)
-        var = jnp.var(mean, axis = -1, keepdims=True)
+        var = jnp.var(x, axis = -1, keepdims=True)
         x_norm = (x-mean)/jnp.sqrt(var + self.eps)
         out = self.gamma * x_norm + self.beta
         return out 
@@ -33,7 +33,7 @@ class FeedForwardNN:
         layer3 = (layer2 @ self.W2) + self.B2
         return layer3
     
-class encoderBlock: 
+class EncoderBlock: 
     def __init__(self, d_model:int, d_ff:int, num_heads:int, key:jax.Array):
         k1, k2, k3, k_ff = random.split(key, 4)
         self.ln1 = LayerNorm(d_model)
@@ -47,7 +47,57 @@ class encoderBlock:
         
         self.d_model = d_model
         self.num_heads = num_heads
+    
+    @jax.jit
+    def __call__(self, x): 
+        x_norm = self.ln1(x)
+        attn_out = multihead_attn(x_norm, x_norm, self.W_q, self.W_k, self.W_v, self.num_heads, self.d_model)
+        x = x + attn_out
+        
+        x_norm2 = self.ln2(x)
+        ffn_out = self.ffnn(x_norm2)
+        x = x + ffn_out
+        return x
+    
+class DecoderBlock:
+    def __init__(self,d_model:int, d_ff:int, num_heads:int, key:jax.Array):
+        k1, k2, k3, k4, k5, k6, k_ff = random.split(key, 7) 
+            
+        self.Wq_mask = random.normal(k1, (d_model, d_model)) *jnp.sqrt(2.0/ d_model)
+        self.Wk_mask = random.normal(k2, (d_model, d_model)) * jnp.sqrt(2.0/d_model)
+        self.Wv_mask = random.normal(k3,(d_model, d_model)) * jnp.sqrt(2.0/d_model)
 
-    def __call__(): 
+        self.Wq_cross = random.normal(k4, (d_model, d_model)) *jnp.sqrt(2.0/ d_model)
+        self.Wk_cross = random.normal(k5, (d_model, d_model)) * jnp.sqrt(2.0/d_model)
+        self.Wv_cross = random.normal(k6,(d_model, d_model)) * jnp.sqrt(2.0/d_model)
+
+        self.ffnn = FeedForwardNN(d_model, d_ff, k_ff)
+        self.ln1 = LayerNorm(d_model)
+        self.ln2 = LayerNorm(d_model)
+        self.ln3 = LayerNorm(d_model)
+        
+        self.d_model = d_model
+        self.num_heads = num_heads
+        
+    def __call__(self, x, enc_out):
+        batch_size, seq_len, _ = x.shape
+        
+        mask = jnp.tril(jnp.ones((seq_len, seq_len), dtype=bool))
+        x1 = self.ln1(x)
+        m_attn_out = masked_multihead_attn(x1, self.Wq_mask, self.Wk_mask, self.Wv_mask, self.num_heads, self.d_model, mask)
+       
+        x = x + m_attn_out
+        x2 = self.ln2(x)
+        c_attn_out = multihead_attn(x2, enc_out, self.Wq_cross, self.Wk_cross, self.Wv_cross, self.num_heads, self.d_model)
+        
+        x = x + c_attn_out
+        x3 = self.ln3(x)
+        x = x + self.ffnn(x3)
+        
+        return x
+
+
+
+
 
 
--- src/selfattention.py
+++ src/selfattention.py
@@ -39,8 +39,49 @@ def init_mheadattn(key:jax.Array, d_model:int, head_dim:int, num_heads:int) -> j
 
 num_heads = 4
 head_dim = 4
+
 @jax.jit
-def multihead_attn(x:jnp.ndarray, W_q:jax.Array, W_k:jax.Array, W_v:jax.Array) -> jnp.ndarray:
+def multihead_attn(q_input: jnp.ndarray, kv_input: jnp.ndarray, W_q: jax.Array, W_k: jax.Array, W_v: jax.Array,
+                   num_heads: int, d_model: int) -> jnp.ndarray:
+    
+    head_dim = d_model // num_heads
+    
+    Q = q_input @ W_q
+    K = kv_input @ W_k
+    V = kv_input @ W_v
+    
+    batch_size, q_len, _ = Q.shape
+    _, kv_len, _ = K.shape
+    
+    Q = Q.reshape(batch_size, q_len, num_heads, head_dim).transpose(0, 2, 1, 3)
+    K = K.reshape(batch_size, kv_len, num_heads, head_dim).transpose(0, 2, 1, 3)
+    V = V.reshape(batch_size, kv_len, num_heads, head_dim).transpose(0, 2, 1, 3)
+    
+    scores = jnp.matmul(Q, K.swapaxes(-2, -1)) / jnp.sqrt(head_dim)
+    weights = softmax(scores, axis=-1)
+    
+    out = jnp.matmul(weights, V)
+    out = out.transpose(0, 2, 1, 3).reshape(batch_size, q_len, d_model)
+    
+    return out
+
+
+key = random.PRNGKey(0)
+batch, seq, d_model, num_heads = 1, 4, 8, 4
+head_dim = d_model // num_heads
+
+x = random.normal(key, (batch, seq, d_model))
+kq, kk, kv = random.split(key, 3)
+W_q = random.normal(kq, (d_model, num_heads * head_dim))
+W_k = random.normal(kk, (d_model, num_heads * head_dim))
+W_v = random.normal(kv, (d_model, num_heads * head_dim))
+
+
+print(jax.make_jaxpr(multihead_attn)(x, W_q, W_k, W_v))
+print(multihead_attn.lower(x, W_q, W_k, W_v).compiler_ir('stablehlo').operation.get_asm())
+
+def masked_multihead_attn(x, W_q, W_k, W_v, num_heads, d_model, mask) -> jnp.ndarray:
+    head_dim = d_model // num_heads 
 
     Q = x @ W_q
     K = x @ W_k
@@ -58,25 +99,20 @@ def multihead_attn(x:jnp.ndarray, W_q:jax.Array, W_k:jax.Array, W_v:jax.Array) -
 
     scores = jnp.matmul(Q, K.swapaxes(-2,-1))/jnp.sqrt(head_dim)
 
+    mask = mask[None, None, :, :]  # -> (1, 1, seq_len, seq_len)
+
+    scores = jnp.where(mask, scores, -1e9)
+
     weights = softmax(scores, axis=-1)
-
     out = weights @ V
-    out = out.transpose(0,2,1,3).reshape(batch_size, seq_len, d_model)
-    return out 
-
-key = random.PRNGKey(0)
-batch, seq, d_model, num_heads = 1, 4, 8, 4
-head_dim = d_model // num_heads
-
-x = random.normal(key, (batch, seq, d_model))
-kq, kk, kv = random.split(key, 3)
-W_q = random.normal(kq, (d_model, num_heads * head_dim))
-W_k = random.normal(kk, (d_model, num_heads * head_dim))
-W_v = random.normal(kv, (d_model, num_heads * head_dim))
+    out = out.transpose(0, 2, 1, 3).reshape(batch_size, seq_len, d_model)
+    return out
 
 
-print(jax.make_jaxpr(multihead_attn)(x, W_q, W_k, W_v))
-print(multihead_attn.lower(x, W_q, W_k, W_v).compiler_ir('stablehlo').operation.get_asm())
+
+
+
+
 
 
 
